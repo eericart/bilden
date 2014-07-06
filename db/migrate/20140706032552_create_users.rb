b3fa1bbdc3eab7cb24d2caf3a243c0f313b7b1cf
Charlin <char0394@gmail.com>
2014-07-06 17:13:04 -0400

    modifying user model

--- db/migrate/20140706032552_create_users.rb
+++ db/migrate/20140706032552_create_users.rb
@@ -6,8 +6,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password
       t.string :email
       t.string :type
-      t.string :carrera
+      t.string :carrear
       t.integer :credits
+      t.integer :extra_credits
 
       t.timestamps
     end
