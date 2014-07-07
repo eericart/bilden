710fe2c4ef77ba8e04ac5e3345f10893ad62b098
Charlin <char0394@gmail.com>
2014-07-06 20:35:39 -0400

    Upload temp files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706222251) do
+ActiveRecord::Schema.define(version: 20140706224125) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
@@ -19,12 +19,15 @@ ActiveRecord::Schema.define(version: 20140706222251) do
     t.string   "password"
     t.string   "email"
     t.string   "type"
-    t.string   "career"
-    t.integer  "extra_credits"
+    t.string   "carrera"
+    t.integer  "credits"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
