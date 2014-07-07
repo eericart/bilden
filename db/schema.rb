58615575f743de6168839e45889c3293dece2cc7
Ernesto Ricart <ernest2193@gmail.com>
2014-07-06 21:58:37 -0400

    _add_password_digest_to_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706233743) do
+ActiveRecord::Schema.define(version: 20140707015701) do
 
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
     t.string   "type"
-    t.string   "carrera"
-    t.integer  "credits"
+    t.string   "career"
+    t.integer  "extra_credits"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
