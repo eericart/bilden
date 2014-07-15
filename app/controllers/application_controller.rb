e3982aff7473f672b072e9f6963fe61ede92539b
Ernesto Ricart <ernest2193@gmail.com>
2014-07-15 16:49:46 -0400

    Remove unused methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,19 +4,4 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   protect_from_forgery
-
-
-  private
-  def check_login
-  if !session[:user_id]
-  	 flash[:status] = FALSE
-     flash[:alert] = 'login first'
- 	 redirect_to login_path
-    else
-    @user = User.find(session[:user_id])
-    $user_first_name = @user.first_name
-    $user_last_name = @user.last_name
-
-   end
-  end
 end
