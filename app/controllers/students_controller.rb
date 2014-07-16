5fe311f277d21e22431ac23ca690b1ec21969f33
Ernesto Ricart <ernest2193@gmail.com>
2014-07-16 11:26:16 -0400

    Fix Conflict

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,10 @@
 class StudentsController < ApplicationController
+  before_action :signed_in_user
   def index
   end
+
+  private
+    def signed_in_user
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,4 @@
 class UsersController < ApplicationController
-
-<<<<<<< HEAD
-=======
-
->>>>>>> de3bb0aedb6dd15913d554a4ac47e8bd70a2d4eb
   def show
   end
 
