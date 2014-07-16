26d4e1872bcf43951521483e4070e184376972fc
Charlin <char0394@gmail.com>
2014-07-16 13:42:07 -0400

    Showing student information

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,18 @@
 class StudentsController < ApplicationController
   before_action :signed_in_user
+  before_action :set_student
+
   def index
+  end
+  def show
   end
 
   private
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def set_student
+      @student = current_user
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :careers
+  resources :students
   resources :sessions, only: [:new, :create, :destroy]
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
