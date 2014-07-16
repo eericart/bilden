9fa79331319f9703079d08038abda32317dd8566
Ernesto Ricart <ernest2193@gmail.com>
2014-07-16 11:11:04 -0400

    Fix bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,19 +10,12 @@ class SessionsController < ApplicationController
       sign_in user
       if user.admin?
         flash[:success] = "Welcome to Bilden App! Admin #{user.first_name}"
-<<<<<<< HEAD
+        redirect_to root_path
+
       else
         flash[:success] = "Welcome to Bilden App #{user.first_name}!"
-=======
-    $user_last_name = @user.last_name
-    redirect_to root_path
-      elsif user[:type] == 'Student'
-        flash[:success] = "Welcome to Bilden App Student #{user.first_name}!"
-       redirect_to dash_student_path
->>>>>>> de3bb0aedb6dd15913d554a4ac47e8bd70a2d4eb
+        redirect_to dash_student_path
       end
-
-      
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
